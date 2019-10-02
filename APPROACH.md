b78a6993af4b47c288e403804a8f6bb337efb2c6
Francesca Chater <francescachater@gmail.com>
2019-10-02 16:16:35 +0100

    Static ball draws on canvas

---
+++ APPROACH.md
@@ -0,0 +1,5 @@
+## Approach
+
+Writing a simple JavaScript function to create and draw a static ball.
+
+![one](images/one.jpg)
--- main.js
+++ main.js
@@ -12,3 +12,19 @@ function random(min, max){
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+function Ball(x, y, velX, velY, color, size){
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function(){
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
